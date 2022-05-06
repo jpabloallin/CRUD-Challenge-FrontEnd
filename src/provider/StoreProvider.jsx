512aba5306f38e3f57f631849a58aca2dce57343
jpabloallin <jpac.647@gmail.com>
2022-05-06 13:24:08 -0500

    Added more Categories and todo by default for testing some functions

--- src/provider/StoreProvider.jsx
+++ src/provider/StoreProvider.jsx
@@ -20,13 +20,31 @@ const initialState = {
       title: "Category by default",
       todos: [
         {
-          id: "1",
+          id: "0",
           name: "to-do by default",
           completed: false,
           fkCategoryId: "0",
         },
       ],
     },
+    {
+        id: "1",
+        title: "Category by default1",
+        todos: [
+          {
+            id: "1",
+            name: "to-do by default1",
+            completed: false,
+            fkCategoryId: "1",
+          },
+          {
+            id: "2",
+            name: "to-do by default2",
+            completed: false,
+            fkCategoryId: "1",
+          },
+        ],
+      },
   ],
 };
 
